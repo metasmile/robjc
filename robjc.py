cf7c20eacd682b12e1f03dbd64a363b102dababa
metasmile <cyrano905@gmail.com>
2015-09-25 16:57:54 +0900

    append class when found subdirs of root path

--- robjc.py
+++ robjc.py
@@ -45,18 +45,22 @@ def begin_objc_interface_file(class_name):
         #import <objc/NSObject.h>
         @class NSString;
 
-        @interface {0} : NSObject
-
         """\
     .format(class_name))
 
 def begin_objc_interface(class_name):
     return textwrap.dedent(
         """\
-        #import <objc/NSObject.h>
-        @class NSString;
-
         @interface {0} : NSObject
+
+        """\
+    .format(class_name))
+
+def begin_objc_implementation_file(class_name):
+    return textwrap.dedent(
+        """\
+        #import <Foundation/Foundation.h>
+        #import \"{0}.h\"
 
         """\
     .format(class_name))
@@ -64,9 +68,6 @@ def begin_objc_interface(class_name):
 def begin_objc_implementation(class_name):
     return textwrap.dedent(
         """\
-        #import <Foundation/Foundation.h>
-        #import \"{0}.h\"
-
         @implementation {0}
 
         """\
@@ -84,6 +85,9 @@ def end_objc_file(path, content):
 def make_static_method_line(name):
     return '+ (NSString *){0};'.format(name)
 
+def make_static_method(content):
+    return make_static_method_line(make_method_content(content))
+
 def make_method_content(content):
     return textwrap.dedent(
         """\
@@ -96,46 +100,82 @@ def make_method_content(content):
 def make_return_string_line(str_content):
     return 'return @\"{0}\";'.format(str_content)
 
+def subdir_class_name(subdir_name):
+    return '{0}_{1}'.format(__RESOURCE_CLASS__, subdir_name) if subdir_name else __RESOURCE_CLASS__
+
 #
 # start main job
 #
 if __name__ == "__main__":
     start = time.time()
 
-    header_file_content = begin_objc_interface(__RESOURCE_CLASS__)
-    impl_file_content = begin_objc_implementation(__RESOURCE_CLASS__)
+    header_file_content = begin_objc_interface_file(__RESOURCE_CLASS__)
+    impl_file_content = begin_objc_implementation_file(__RESOURCE_CLASS__)
+
     cnt = 0
 
     def append_static_file(file_name, file):
         _method_line = make_static_method_line(file_name)
-
         global header_file_content
         global impl_file_content
         header_file_content += (_method_line + '\n\n')
         impl_file_content += (_method_line + make_method_content(make_return_string_line(file)) + '\n')
 
-    def append_subdir(dirname):
+    def append_class(class_name):
         global header_file_content
         global impl_file_content
+        header_file_content += begin_objc_interface(class_name)
+        impl_file_content += begin_objc_implementation(class_name)
+
+    def append_subdirs(subdirs):
+        #root
+        for subdir in subdirs:
+            header_file_content += begin_objc_interface(subdir)
+            impl_file_content += begin_objc_implementation(subdir)
+
+            impl_file_content += textwrap.dedent(
+                """\
+                + ({rcls} *){0}; {{
+                    static {rcls} * instance_{0};
+                    static dispatch_once_t onceToken_{0};
+                    dispatch_once(&onceToken_{0}, ^{{
+                        instance_{0} = [[{rcls} alloc] init];
+                    }});
+                    return instance_{0};
+                }}
+                """\
+            .format(dirname,rcls=subdir_class_name(dirname))) + '\n'
+
+    def append_subdir_class_defines(subdir_name):
+        _subclass_name = subdir_class_name(subdir_name)
+
+        global header_file_content
+        global impl_file_content
+        header_file_content += (make_static_method_line(subdir_name/) + '\n\n')
         impl_file_content += textwrap.dedent(
             """\
-            + (R_{0} *){0}; {{
-                static R_{0} * instance_{0};
+            + ({rcls} *){0}; {{
+                static {rcls} * instance_{0};
                 static dispatch_once_t onceToken_{0};
                 dispatch_once(&onceToken_{0}, ^{{
-                    instance_{0} = [[R_{0} alloc] init];
+                    instance_{0} = [[{rcls} alloc] init];
                 }});
                 return instance_{0};
             }}
             """\
-        .format(dirname)) + '\n'
+        .format(subdir_name,rcls=_subclass_name)) + '\n'
 
-    for root, dirs, files in os.walk(__RESOURCE_PATH__):
+    for root, dirs, files in reversed(list(os.walk(__RESOURCE_PATH__))):#os.walk(__RESOURCE_PATH__):
         cur_dir = os.path.basename(root)
         if not cur_dir:
             continue
 
-        [[ append_subdir(_subdir) ] for _subdir in dirs]
+        print '----',dirs if dirs else 'A'
+
+        # append_subdirs(dirs)
+        append_class(subdir_class_name(cur_dir if root!=__RESOURCE_PATH__ else None))
+
+        [[ append_subdir_class_defines(_subdir) ] for _subdir in dirs]
 
         for file_name in files:
             file = os.path.join(root, file_name)
